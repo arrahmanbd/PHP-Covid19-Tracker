37f15379a756b1a779617835efe3c3da160bdc79
AR Rahman <74553735+arrahmanbd@users.noreply.github.com>
2021-04-08 17:49:33 +0600

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,21 @@
+<?php
+
+$title = "Coronavirus Tracker - Covid-19 Rate, Map, Country Data";
+
+$mapAPI = "AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY"; //Note: you will need to get a mapsApiKey for your project. See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+
+$ads = "<a href='https://codecanyon.net/user/nodetent/portfolio' target='_blank'><img src='assets/images/banner.png' style='width: 100%;' /></a>";
+
+$language = "en";
+/*
+en - English
+ar - Arabic
+ch - Chineese/Mandarin
+es - Spanish
+fr - French
+hi - Hidi
+kr - Korean
+pt - Portuguese
+ru - Russian
+*/
+?>
